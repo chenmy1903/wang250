aab0ce12cb7bb22db9c6b840da4f07daf5480828
chenmy1903 <chenmy10@outlook.com>
2022-01-08 21:26:42 +0800

    Signed-off-by: chenmy1903 <chenmy10@outlook.com>

--- play/main.py
+++ play/main.py
@@ -806,7 +806,7 @@ class Surf(Text):
             self.blit_text("鸭皇游戏 | 逃离王建国", (window_info.current_w / 2 - 72 * 5, window_info.current_h / 2 - 100), 72, pygame.Color(255, 255, 255))
             self.DISPLAYSURF.blit(logo, (window_info.current_w / 4 - 72 * 5, window_info.current_h / 2 - 100))
             self.blit_text(f"下载资源 进度：{process * 100}%", (window_info.current_w / 2 - 72 * 5, window_info.current_h - 100), 72, pygame.Color(255, 255, 255))
-            if precess == 1:
+            if process == 1:
                 return
             for event in pygame.event.get():
                 if event.type == QUIT:
