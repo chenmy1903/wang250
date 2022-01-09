7d0f44aa2ee375f39d36c7cad81906ebbf58d816
chenmy1903 <chenmy10@outlook.com>
2022-01-09 16:13:57 +0800

    Signed-off-by: chenmy1903 <chenmy10@outlook.com>

--- play/game.py
+++ play/game.py
@@ -203,7 +203,7 @@ class RunGame(Text):
         
         w = self.win_info.current_w
         h = self.win_info.current_h
-        self.y -= 16 if self.y > 50
+        self.y -= 16 if self.y > 50 else 0
         self.jump = False
 
     def start(self):
