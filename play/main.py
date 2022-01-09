5f9053144edc8db9eaa11db9347b472d89947b4d
chenmy1903 <chenmy10@outlook.com>
2022-01-09 09:06:40 +0800

    Signed-off-by: chenmy1903 <chenmy10@outlook.com>

--- play/main.py
+++ play/main.py
@@ -807,7 +807,7 @@ class Surf(Text):
             process = self.download_file_count / len(paths) - len(load_dir_list)
             self.blit_text("鸭皇游戏 | 逃离王建国", (window_info.current_w / 2 - 72 * 5, window_info.current_h / 2 - 100), 72, pygame.Color(255, 255, 255))
             self.DISPLAYSURF.blit(logo, (window_info.current_w / 4 - 72 * 5, window_info.current_h / 2 - 100))
-            self.blit_text(f"下载资源 进度：{process * 100}%", (window_info.current_w / 2 - 72 * 5, window_info.current_h - 100), 72, pygame.Color(255, 255, 255))
+            self.blit_text(f"下载资源 进度：{int(process)}%", (window_info.current_w / 2 - 72 * 5, window_info.current_h - 100), 72, pygame.Color(255, 255, 255))
             if process >= 1:
                 return
             for event in pygame.event.get():
